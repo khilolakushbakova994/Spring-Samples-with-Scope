0cee2acb1664a7818f73a990a292d12246faf437
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-12-03 13:48:02 +0200

    Initial commit

---
+++ src/main/java/com/example/Spring/Samples/with/Scope/SpringSamplesWithScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Samples.with.Scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSamplesWithScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSamplesWithScopeApplication.class, args);
+	}
+
+}
